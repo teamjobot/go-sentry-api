8c9167efdeb5c1773854dd796f01dbdc6b1daec2
Geoff Hudik <120529+thnk2wn@users.noreply.github.com>
2023-01-24 10:01:10 -0500

    JAX-124: go-sentry-api module name to jobot

--- events.go
+++ events.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/atlassian/go-sentry-api/datatype"
+	"github.com/teamjobot/go-sentry-api/datatype"
 )
 
 // Tag is used for a event
@@ -14,7 +14,7 @@ type Tag struct {
 	Key   *string `json:"key,omitempty"`
 }
 
-//User is the user that was affected
+// User is the user that was affected
 type User struct {
 	Username *string  `json:"username,omitempty"`
 	Email    *string  `json:"email,omitempty"`
@@ -29,7 +29,7 @@ type Entry struct {
 	Data json.RawMessage `json:"data,omitempty"`
 }
 
-//GetInterface will convert the entry into a go interface
+// GetInterface will convert the entry into a go interface
 func (e *Entry) GetInterface() (string, interface{}, error) {
 	var destination interface{}
 
@@ -88,14 +88,14 @@ func (c *Client) GetProjectEvent(o Organization, p Project, eventID string) (Eve
 	return event, err
 }
 
-//GetLatestEvent will fetch the latest event for a issue
+// GetLatestEvent will fetch the latest event for a issue
 func (c *Client) GetLatestEvent(i Issue) (Event, error) {
 	var event Event
 	err := c.do("GET", fmt.Sprintf("issues/%s/events/latest", *i.ID), &event, nil)
 	return event, err
 }
 
-//GetOldestEvent will fetch the latest event for a issue
+// GetOldestEvent will fetch the latest event for a issue
 func (c *Client) GetOldestEvent(i Issue) (Event, error) {
 	var event Event
 	err := c.do("GET", fmt.Sprintf("issues/%s/events/oldest", *i.ID), &event, nil)
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/atlassian/go-sentry-api
+module github.com/teamjobot/go-sentry-api
 
 go 1.13
 
